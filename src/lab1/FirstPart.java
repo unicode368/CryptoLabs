17cc3d39be88e0d47f0c50d91849367857952e54
lamecode <kislaya.sliva@gmail.com>
2021-12-17 14:14:24 +0200

    without xor

--- src/lab1/FirstPart.java
+++ src/lab1/FirstPart.java
@@ -3,5 +3,14 @@ package lab1;
 public class FirstPart implements EncryptedPart {
     @Override
     public void process(String encryptedText) {
+        char[] chars = encryptedText.toCharArray();
+        for (int i = 0; i < 26; i++) {
+            String a = "";
+            for (int j = 0; j < chars.length; j++){
+                a += chars[j]--;
+            }
+            System.out.println(a);
+        }
+        //System.out.println(encryptedText);
     }
 }
