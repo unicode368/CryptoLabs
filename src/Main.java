a58b8f3cb8bef6d4b9d0dfe20728cac6d0967b60
lamecode <kislaya.sliva@gmail.com>
2021-12-17 13:16:51 +0200

    binary string to text

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         String stringBytes = FileReader
                 .extractRawData("1lab.txt", Charset.defaultCharset());
+        StringBuilder sb = new StringBuilder();
+
+        Arrays.stream(stringBytes.split("(?<=\\G.{8})"))
+                .forEach(s -> sb.append((char) Integer.parseInt(s, 2)));
+
+        String output = sb.toString();
+        System.out.println(output);
     }
 }
