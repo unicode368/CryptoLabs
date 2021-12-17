9c4eeada075b98527406e8188e44fc42c3a0471c
lamecode <kislaya.sliva@gmail.com>
2021-12-17 22:40:35 +0200

    same as for part one

--- src/lab1/SecondPart.java
+++ src/lab1/SecondPart.java
@@ -3,6 +3,13 @@ package lab1;
 public class SecondPart implements EncryptedPart{
     @Override
     public void process(String encryptedText) {
-       // System.out.println(encryptedText);
+        char[] chars = encryptedText.toCharArray();
+        for (int i = 0; i < 256; i++) {
+            for (int j = 0; j < chars.length; j++) {
+                chars[j] = (char) (chars[j] ^ i);
+            }
+            System.out.println(chars);
+            chars = encryptedText.toCharArray();
+        }
     }
 }
