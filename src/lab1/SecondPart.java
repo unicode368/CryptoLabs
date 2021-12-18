823fe4ab4442d55a0fb35124da70ce6ab30165b2
lamecode <kislaya.sliva@gmail.com>
2021-12-18 23:31:21 +0200

    switching to hashmap

--- src/lab1/SecondPart.java
+++ src/lab1/SecondPart.java
@@ -4,18 +4,22 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.HashMap;
 
 public class SecondPart {
     public void process(String encryptedText, String hint) {
         encryptedText = new String(Base64.getDecoder().decode(encryptedText));
         char[] chars = encryptedText.toCharArray();
         //searching for bigrams
-        ArrayList<String> bigrams = new ArrayList<>();
+        HashMap<String, ArrayList<Integer>> bigramsAndOccurences =
+                new HashMap<>();
+        //ArrayList<String> bigrams = new ArrayList<>();
         ArrayList<String> trigrams = new ArrayList<>();
         for (int i = 0; i < encryptedText.length() - 2; i++) {
             String bigram = encryptedText.substring(i, i + 2);
-            if (countMatches(encryptedText, bigram).size() > 1) {
-                bigrams.add(bigram);
+            ArrayList<Integer> occurences = countMatches(encryptedText, bigram);
+            if (occurences.size() > 1) {
+                bigramsAndOccurences.put(bigram, occurences);
             }
         }
         for (int i = 0; i < encryptedText.length() - 3; i++) {
@@ -24,8 +28,9 @@ public class SecondPart {
                 trigrams.add(bigram);
             }
         }
-        for (String str : trigrams) {
-            System.out.print(str + " ");
+        for (String name: bigramsAndOccurences.keySet()) {
+            String value = bigramsAndOccurences.get(name).toString();
+            System.out.println(name + " - " + value);
         }
         /*String key = "a";
         for (int i = 0; i < chars.length; i++) {
