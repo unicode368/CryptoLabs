587c59a100db105fccc8dd7f2a1fcaf189ae1181
lamecode <kislaya.sliva@gmail.com>
2021-12-18 23:24:29 +0200

    searching for bigrams and trigrams (Kasiski approach)

--- src/lab1/SecondPart.java
+++ src/lab1/SecondPart.java
@@ -9,13 +9,48 @@ public class SecondPart {
     public void process(String encryptedText, String hint) {
         encryptedText = new String(Base64.getDecoder().decode(encryptedText));
         char[] chars = encryptedText.toCharArray();
-        String key = "a";
+        //searching for bigrams
+        ArrayList<String> bigrams = new ArrayList<>();
+        ArrayList<String> trigrams = new ArrayList<>();
+        for (int i = 0; i < encryptedText.length() - 2; i++) {
+            String bigram = encryptedText.substring(i, i + 2);
+            if (countMatches(encryptedText, bigram).size() > 1) {
+                bigrams.add(bigram);
+            }
+        }
+        for (int i = 0; i < encryptedText.length() - 3; i++) {
+            String bigram = encryptedText.substring(i, i + 3);
+            if (countMatches(encryptedText, bigram).size() > 1) {
+                trigrams.add(bigram);
+            }
+        }
+        for (String str : trigrams) {
+            System.out.print(str + " ");
+        }
+        /*String key = "a";
         for (int i = 0; i < chars.length; i++) {
             chars[i] = (char) (chars[i] ^ key.charAt(i % key.length()));
         }
-        System.out.println(chars);
+        System.out.println(chars);*/
         //System.out.println(encryptedText);
         //System.out.println(hint);
+    }
+
+    public static ArrayList<Integer> countMatches(String text, String str) {
+        ArrayList<Integer> indexes = new ArrayList<>();
+        int index = 0;
+        while (true) {
+            index = text.indexOf(str, index);
+            if (index != -1) {
+                indexes.add(index);
+                index += str.length();
+            }
+            else {
+                break;
+            }
+        }
+
+        return indexes;
     }
 
 
