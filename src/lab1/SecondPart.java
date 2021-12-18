3a2ab81c855190e944936e2faa9fa0a063094604
lamecode <kislaya.sliva@gmail.com>
2021-12-18 22:26:15 +0200

    decoding.

--- src/lab1/SecondPart.java
+++ src/lab1/SecondPart.java
@@ -1,12 +1,22 @@
 package lab1;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
 
 public class SecondPart {
     public void process(String encryptedText, String hint) {
         encryptedText = new String(Base64.getDecoder().decode(encryptedText));
-        System.out.println(encryptedText);
-        System.out.println(hint);
+        char[] chars = encryptedText.toCharArray();
+        String key = "a";
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = (char) (chars[i] ^ key.charAt(i % key.length()));
+        }
+        System.out.println(chars);
+        //System.out.println(encryptedText);
+        //System.out.println(hint);
     }
+
+
 }
