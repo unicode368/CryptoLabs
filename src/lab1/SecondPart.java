7a89888fe9d85e62b43696447dc76215f613e5c0
lamecode <kislaya.sliva@gmail.com>
2021-12-18 19:49:37 +0200

    decrypting from base64 due to '=' sign at the end

--- src/lab1/SecondPart.java
+++ src/lab1/SecondPart.java
@@ -1,9 +1,11 @@
 package lab1;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class SecondPart {
     public void process(String encryptedText, String hint) {
+        encryptedText = new String(Base64.getDecoder().decode(encryptedText));
         System.out.println(encryptedText);
         System.out.println(hint);
     }
