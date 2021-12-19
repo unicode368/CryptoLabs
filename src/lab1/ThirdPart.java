1f82c244e6ebe4f5e0cfcfedfedf0ab54e90ae56
lamecode <kislaya.sliva@gmail.com>
2021-12-19 14:04:05 +0200

    alphabet + basic values for genetic algorithm

---
+++ src/lab1/ThirdPart.java
@@ -0,0 +1,15 @@
+package lab1;
+
+public class ThirdPart {
+    final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private int populationSize = 0;
+    private int topPopulation = 0;
+    private int stabilityIntervals = 0;
+    private int crossoverCount = 0;
+    private int mutationsCount = 0;
+
+    public String process(String encryptedText) {
+        return "";
+    }
+    
+}
