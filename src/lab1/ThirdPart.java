5e8fbe9d8ab2945c6ccaf02de71e94b3901b3a70
lamecode <kislaya.sliva@gmail.com>
2021-12-21 21:30:39 +0200

    generate random alphabet

--- src/lab1/ThirdPart.java
+++ src/lab1/ThirdPart.java
@@ -1,6 +1,7 @@
 package lab1;
 
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ThirdPart {
     final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -13,14 +14,33 @@ public class ThirdPart {
     public String process(String encryptedText) {
         char[] chars = encryptedText.toCharArray();
         HashMap<Character, Character> decipherPairs = getDecipherPairs();
-        for (int i = 0; i < chars.length; i++) {
+        Set<String> alphabets = new HashSet<>();
+        getRandomAlphabet();
+        /*for (int i = 0; i < chars.length; i++) {
             chars[i] = decipherPairs.get(chars[i]);
-        }
+        }*/
         return String.valueOf(chars);
     }
 
     public HashMap<Character, Character> getDecipherPairs() {
         return new HashMap<>();
+    }
+
+    public String getRandomAlphabet() {
+        String newAlphabet = "";
+        ArrayList<Character> tempChars = new ArrayList<>(
+                alphabet.chars()
+                        .mapToObj(e -> (char) e)
+                        .collect(
+                                Collectors.toList()
+                        )
+        );
+        while (!tempChars.isEmpty()) {
+            int choosenChar = new Random().nextInt(tempChars.size());
+            newAlphabet += tempChars.get(choosenChar);
+            tempChars.remove(choosenChar);
+        }
+        return newAlphabet;
     }
 
 }
