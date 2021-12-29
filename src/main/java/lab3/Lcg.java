b98abe8b6eeeaf31163bd4a65c428104d2d51e03
lamecode <kislaya.sliva@gmail.com>
2021-12-29 16:42:33 +0200

    doing some research...

--- src/main/java/lab3/Lcg.java
+++ src/main/java/lab3/Lcg.java
@@ -1,4 +1,5 @@
 package lab3;
 
 public class Lcg {
+
 }
--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -4,12 +4,30 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        new API().createAccount("63467");
-        //{"message":"You lost this time","account":
-        // {"id":"63467","money":999,"deletionTime":"2021-12-27T23:36:59.341588Z"},"realNumber":186041942}
-        //{"message":"You lost this time","account":{"id":"63467","money":998,
-        // "deletionTime":"2021-12-27T23:36:59.341588Z"},"realNumber":1040402877}
-        //{"message":"You lost this time","account":{"id":"63467","money":997,
-        // "deletionTime":"2021-12-27T23:36:59.341588Z"},"realNumber":-445644808}
+        //new API().createAccount("63467");
+        new API().play("63467", 1, 352);
+        //x0 : 984452904
+        //x1 : 1496393831
+        //x2 : 1276512922
+        long a1 = 984452904 - 1496393831;
+        long a2 = 1276512922 - 1496393831;
+        int kInt = Integer.MAX_VALUE + (Integer.MAX_VALUE + 2 - 1616625442);
+        long k = (long) Integer.MAX_VALUE + (Integer.MAX_VALUE + 2 - 529151308);
+        System.out.println(kInt);
+        System.out.println(k);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        //System.out.println(Math.pow(2, 32));
+        //System.out.println(a1);
+        //System.out.println(a2);
+        //System.out.println((long) 1040402877 - 1664525 * a1);
+        //System.out.println(a2 % Math.pow(2, 32));
+
+        //long inverse_a1 = 0;
+        //long a = (inverse_a1 * a1) / (long) Math.pow(2, 32);
+        //System.out.println(a);
     }
+
+
+
 }
