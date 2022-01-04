628145d1601fb9f5f713775f9d19afcdefea1db2
lamecode <kislaya.sliva@gmail.com>
2022-01-05 00:27:39 +0200

    top 100 password probability

--- src/main/java/lab4/PasswordGenerator.java
+++ src/main/java/lab4/PasswordGenerator.java
@@ -5,4 +5,6 @@ import java.util.Random;
 public class PasswordGenerator {
     final double top1MpasswordProbability
             = (50 + new Random().nextInt(41)) / 100;
+    final double top100passwordProbability
+            = (5 + new Random().nextInt(6)) / 100;
 }
