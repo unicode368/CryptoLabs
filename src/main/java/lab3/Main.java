3a8f545d1b1bb13f3475af5eb8010d18342d895a
lamecode <kislaya.sliva@gmail.com>
2022-01-05 00:18:26 +0200

    no progress.

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -5,27 +5,28 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //new API().createAccount("63467");
-        new API().play("63467", 1, 352);
-        //x0 : 984452904
-        //x1 : 1496393831
-        //x2 : 1276512922
-        long a1 = 984452904 - 1496393831;
-        long a2 = 1276512922 - 1496393831;
+        //new API().play("63467", 1, 352);
+        //x0 : 1748853791
+        //x1 : -998743310
+        //x2 : -677747287
+        //{"realNumber":-1589087500,"message":"You lost this time","account":
+        // {"money":981,"deletionTime":"2021-12-29T15:26:23.0928168Z","id":"63467"}}
         int kInt = Integer.MAX_VALUE + (Integer.MAX_VALUE + 2 - 1616625442);
-        long k = (long) Integer.MAX_VALUE + (Integer.MAX_VALUE + 2 - 529151308);
-        System.out.println(kInt);
+        long k = (long) Integer.MAX_VALUE + (Integer.MAX_VALUE + 2 - 1616625442);
+        int x0 = 1748853791;
+        int x1 = Integer.MAX_VALUE + (Integer.MAX_VALUE + 2 - 998743310);
+        int x2 = Integer.MAX_VALUE + (Integer.MAX_VALUE + 2 - 677747287);
+        int a1 = x0 - x1;
+        int a2 = x1 - x2;
+        /*System.out.println(kInt);
         System.out.println(k);
         System.out.println(Long.MAX_VALUE);
-        System.out.println(Integer.MAX_VALUE);
-        //System.out.println(Math.pow(2, 32));
-        //System.out.println(a1);
-        //System.out.println(a2);
-        //System.out.println((long) 1040402877 - 1664525 * a1);
-        //System.out.println(a2 % Math.pow(2, 32));
-
-        //long inverse_a1 = 0;
-        //long a = (inverse_a1 * a1) / (long) Math.pow(2, 32);
-        //System.out.println(a);
+        System.out.println(Integer.MAX_VALUE);*/
+        System.out.println(x0);
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(a1);
+        System.out.println(a2);
     }
 
 
