7e3513aeae404d9f675d1d69880a813a9eaf1ddd
lamecode <kislaya.sliva@gmail.com>
2022-01-06 22:32:51 +0200

    retrieving third part

--- src/main/java/lab4/PasswordStorage.java
+++ src/main/java/lab4/PasswordStorage.java
@@ -72,6 +72,7 @@ public class PasswordStorage {
     public static String genHumanLikePassword() {
         String first = firstPart[new Random().nextInt(firstPart.length)];
         String second = secondPart[new Random().nextInt(secondPart.length)];
+        String third = thirdPart[new Random().nextInt(thirdPart.length)];
     }
 
 
