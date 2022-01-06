ecaabd5c5ffcb894cbb0a1404b50cdbc6319ba30
lamecode <kislaya.sliva@gmail.com>
2022-01-06 20:40:23 +0200

    variable to return

--- src/main/java/lab4/PasswordStorage.java
+++ src/main/java/lab4/PasswordStorage.java
@@ -1,5 +1,6 @@
 package lab4;
 
+
 public class PasswordStorage {
     enum StorageType {
         TOP1M,
@@ -9,8 +10,10 @@ public class PasswordStorage {
     }
 
     public static String getPassword(StorageType storageType) {
+        String password;
         switch (storageType) {
-            case TOP1M: break;
+            case TOP1M:
+
             case TOP100: break;
             case REALLY_RANDOM: break;
             case MY_PASSWORD: break;
