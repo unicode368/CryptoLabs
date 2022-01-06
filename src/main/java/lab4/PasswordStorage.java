951c3894d9d61da50af056c3fa0a1ae95460684b
lamecode <kislaya.sliva@gmail.com>
2022-01-06 20:48:56 +0200

    random password number

--- src/main/java/lab4/PasswordStorage.java
+++ src/main/java/lab4/PasswordStorage.java
@@ -1,6 +1,11 @@
 package lab4;
 
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Random;
+import java.util.stream.Stream;
+
 public class PasswordStorage {
     enum StorageType {
         TOP1M,
@@ -13,7 +18,8 @@ public class PasswordStorage {
         String password;
         switch (storageType) {
             case TOP1M:
-
+                int passwordNum = 1 + new Random().nextInt(1000000);
+                
             case TOP100: break;
             case REALLY_RANDOM: break;
             case MY_PASSWORD: break;
