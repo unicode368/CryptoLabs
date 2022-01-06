1790037ffc3be6ef67b8c7369be5314fb971a104
lamecode <kislaya.sliva@gmail.com>
2022-01-06 20:55:19 +0200

    getting password from 1M file

--- src/main/java/lab4/PasswordStorage.java
+++ src/main/java/lab4/PasswordStorage.java
@@ -1,6 +1,5 @@
 package lab4;
 
-
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Random;
@@ -19,7 +18,10 @@ public class PasswordStorage {
         switch (storageType) {
             case TOP1M:
                 int passwordNum = 1 + new Random().nextInt(1000000);
-                
+                try (Stream<String> lines = Files.lines(Paths.get("file.txt"))) {
+                    password = lines.skip(passwordNum).findFirst().get();
+                }
+                return password;
             case TOP100: break;
             case REALLY_RANDOM: break;
             case MY_PASSWORD: break;
