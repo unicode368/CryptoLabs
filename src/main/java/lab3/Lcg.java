3c2765ed0274d1ca4234e57391c8d9e07d11f5a8
lamecode <kislaya.sliva@gmail.com>
2022-01-08 22:53:13 +0200

    make public

--- src/main/java/lab3/Lcg.java
+++ src/main/java/lab3/Lcg.java
@@ -3,7 +3,7 @@ package lab3;
 import java.math.BigInteger;
 
 public class Lcg {
-    static final long m = (long) Math.pow(2, 32);
+    public static final long m = (long) Math.pow(2, 32);
     int a;
     int c;
     int last;
