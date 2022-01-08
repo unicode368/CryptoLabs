519cef6c755e415fde931bf9a0b8351a95c06253
lamecode <kislaya.sliva@gmail.com>
2022-01-08 22:51:16 +0200

    lcg attributes + constructor

--- src/main/java/lab3/Lcg.java
+++ src/main/java/lab3/Lcg.java
@@ -1,5 +1,17 @@
 package lab3;
 
-public class Lcg {
+import java.math.BigInteger;
 
+public class Lcg {
+    static final long m = (long) Math.pow(2, 32);
+    int a;
+    int c;
+    int last;
+
+    Lcg(int current, int a, int c) {
+        this.a = a;
+        this.c = c;
+        last = current;
+    }
+    
 }
