b7c403ee30aa5876861ebe877342ebac6b23d103
lamecode <kislaya.sliva@gmail.com>
2022-01-08 15:05:46 +0200

    new value

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -6,27 +6,53 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //new API().createAccount("63467");
         //new API().play("63467", 1, 352);
-        //x0 : 1748853791
-        //x1 : -998743310
-        //x2 : -677747287
-        //{"realNumber":-1589087500,"message":"You lost this time","account":
-        // {"money":981,"deletionTime":"2021-12-29T15:26:23.0928168Z","id":"63467"}}
-        int kInt = Integer.MAX_VALUE + (Integer.MAX_VALUE + 2 - 1616625442);
-        long k = (long) Integer.MAX_VALUE + (Integer.MAX_VALUE + 2 - 1616625442);
-        int x0 = 1748853791;
-        int x1 = Integer.MAX_VALUE + (Integer.MAX_VALUE + 2 - 998743310);
-        int x2 = Integer.MAX_VALUE + (Integer.MAX_VALUE + 2 - 677747287);
-        int a1 = x0 - x1;
-        int a2 = x1 - x2;
+        System.out.println();
+        //{"money":1000,"deletionTime":"2022-01-08T14:03:50.9633993Z","id":"63467"}
+        //{"realNumber":-2029718102,"message":"You lost this time","account":{"money":999,
+        //        "deletionTime":"2022-01-08T14:03:50.9633993Z","id":"63467"}}
+        /*long a = (-677747287 + 998743310) * (modInverse((long) -998743310 - 1748853791,
+                (long) Math.pow(2, 32))) % (long) Math.pow(2, 32);
+        System.out.println(a);*/
         /*System.out.println(kInt);
         System.out.println(k);
         System.out.println(Long.MAX_VALUE);
         System.out.println(Integer.MAX_VALUE);*/
-        System.out.println(x0);
+        /*System.out.println(x0);
         System.out.println(x1);
         System.out.println(x2);
         System.out.println(a1);
-        System.out.println(a2);
+        System.out.println(a2);*/
+    }
+
+    static long modInverse(long a, long m) {
+        long m0 = m;
+        long y = 0, x = 1;
+
+        if (m == 1)
+            return 0;
+
+        while (a > 1) {
+            // q is quotient
+            long q = a / m;
+
+            long t = m;
+
+            // m is remainder now, process
+            // same as Euclid's algo
+            m = a % m;
+            a = t;
+            t = y;
+
+            // Update x and y
+            y = x - q * y;
+            x = t;
+        }
+
+        // Make x positive
+        if (x < 0)
+            x += m0;
+
+        return x;
     }
 
 
