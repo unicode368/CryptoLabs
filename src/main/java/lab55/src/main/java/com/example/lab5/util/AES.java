1237c4654c488a0e545b837a93fab75434ae1585
lamecode <kislaya.sliva@gmail.com>
2022-01-11 16:49:14 +0200

    decrypt method

--- src/main/java/lab55/src/main/java/com/example/lab5/util/AES.java
+++ src/main/java/lab55/src/main/java/com/example/lab5/util/AES.java
@@ -94,4 +94,17 @@ public class AES {
         return Base64.getEncoder()
                 .encodeToString(cipherText);
     }
+
+    //in case if application contains pages which require this encrypted info
+    public static String decrypt(String algorithm, String cipherText, SecretKey key,
+                                 IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        byte[] plainText = cipher.doFinal(Base64.getDecoder()
+                .decode(cipherText));
+        return new String(plainText);
+    }
 }
