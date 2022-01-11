0fa66205f7373467372b447c25ed1a146aa30c6d
lamecode <kislaya.sliva@gmail.com>
2022-01-11 13:08:20 +0200

    read file

--- src/main/java/lab55/src/main/java/com/example/lab5/util/AES.java
+++ src/main/java/lab55/src/main/java/com/example/lab5/util/AES.java
@@ -1,21 +1,33 @@
 package com.example.lab5.util;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.objenesis.SpringObjenesis;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AES {
     public static String getPassword(PasswordType passwordType) {
-        File passwords
+        ArrayList<String> passwords = processFile("resources/my_config.txt");
         return "";
     }
 
-    public static String[] processFile(String location) {
+    public static ArrayList<String> processFile(String location) {
         try {
-
+            List<String> lines = Files.readAllLines(Paths.get(location),
+                    Charset.defaultCharset());
+            return (ArrayList<String>) lines;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return new ArrayList<>();
     }
 }
