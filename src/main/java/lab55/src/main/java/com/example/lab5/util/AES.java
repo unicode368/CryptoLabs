68693f1f9bb0acf9797fdb971b08f417ce575c2f
lamecode <kislaya.sliva@gmail.com>
2022-01-11 10:40:29 +0200

    exception scope

--- src/main/java/lab55/src/main/java/com/example/lab5/util/AES.java
+++ src/main/java/lab55/src/main/java/com/example/lab5/util/AES.java
@@ -3,6 +3,7 @@ package com.example.lab5.util;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
+import java.io.IOException;
 
 public class AES {
     public static String getPassword(PasswordType passwordType) {
@@ -11,6 +12,10 @@ public class AES {
     }
 
     public static String[] processFile(String location) {
+        try {
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
