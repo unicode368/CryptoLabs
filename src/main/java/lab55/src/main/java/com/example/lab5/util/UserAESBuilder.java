a1b9c2797fcc92d813e4820b69ad89961aefe2dc
lamecode <kislaya.sliva@gmail.com>
2022-01-11 17:31:50 +0200

    aes builder

---
+++ src/main/java/lab55/src/main/java/com/example/lab5/util/UserAESBuilder.java
@@ -0,0 +1,20 @@
+package com.example.lab5.util;
+
+public class UserAESBuilder {
+    String surname;
+    String name;
+    String patronimic;
+    String email;
+    String phoneNumber;
+    String salt1;
+    String salt2;
+    String salt3;
+    String salt4;
+    String salt5;
+    String vectorIV;
+
+    UserAESBuilder(String surname, String name, String patronimic,
+                   String email, String phoneNumber) {
+        
+    }
+}
