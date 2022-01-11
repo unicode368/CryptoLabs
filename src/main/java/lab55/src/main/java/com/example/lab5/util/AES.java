94f2348094a51540e0b740fa91de88799ef3bff4
lamecode <kislaya.sliva@gmail.com>
2022-01-11 16:43:38 +0200

    generate secret from "password" and salt

--- src/main/java/lab55/src/main/java/com/example/lab5/util/AES.java
+++ src/main/java/lab55/src/main/java/com/example/lab5/util/AES.java
@@ -76,7 +76,11 @@ public class AES {
 
     public static SecretKey getKeyFromPassword(String password, String salt)
             throws NoSuchAlgorithmException, InvalidKeySpecException {
-
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
+        SecretKey secret = new SecretKeySpec(factory.generateSecret(spec)
+                .getEncoded(), "AES");
+        return secret;
     }
 
     public static String encrypt(String algorithm, String input, SecretKey key,
