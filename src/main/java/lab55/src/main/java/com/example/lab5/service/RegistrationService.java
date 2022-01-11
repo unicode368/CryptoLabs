d161b4b02c5e362f08fe0e348af753c5e1479271
lamecode <kislaya.sliva@gmail.com>
2022-01-11 18:50:22 +0200

    register encrypted data

--- src/main/java/lab55/src/main/java/com/example/lab5/service/RegistrationService.java
+++ src/main/java/lab55/src/main/java/com/example/lab5/service/RegistrationService.java
@@ -4,10 +4,19 @@ import com.example.lab5.dto.UserDTO;
 import com.example.lab5.entity.User;
 import com.example.lab5.entity.UserInfo;
 import com.example.lab5.exceptions.UserAlreadyExistsException;
+import com.example.lab5.util.UserAESBuilder;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 
 @Service
@@ -19,11 +28,22 @@ public class RegistrationService {
 
     public User register(@RequestBody UserDTO request,
                          String roleName) throws UserAlreadyExistsException {
-        return userService.signUpUser(new User(
-                request.getLogin(),
-                        bCryptPasswordEncoder.encode(request.getPassword())),
-                new UserInfo(request.getLastName(),
-                        request.getFirstName(), request.getPatronimic(),
-                        request.getPhoneNumber(), request.getEmail()), roleName);
+        try {
+            UserAESBuilder encryptedData = new UserAESBuilder(request.getLastName(),
+                    request.getFirstName(), request.getPatronimic(),
+                    request.getEmail(), request.getPhoneNumber());
+            return userService.signUpUser(new User(
+                            request.getLogin(),
+                            bCryptPasswordEncoder.encode(request.getPassword())),
+                    new UserInfo(encryptedData.getSurname(),
+                            encryptedData.getName(), encryptedData.getPatronimic(),
+                            encryptedData.getPhoneNumber(), encryptedData.getEmail()), roleName);
+        } catch (InvalidKeySpecException |
+                NoSuchAlgorithmException | IllegalBlockSizeException
+                | InvalidKeyException | BadPaddingException | InvalidAlgorithmParameterException
+                | NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+        return new User();
     }
 }
