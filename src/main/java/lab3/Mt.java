217e64ffbca42362437d09a4a6c229f4b994ad9e
lamecode <kislaya.sliva@gmail.com>
2022-01-14 09:06:05 +0200

    another constants

--- src/main/java/lab3/Mt.java
+++ src/main/java/lab3/Mt.java
@@ -2,6 +2,17 @@ package lab3;
 
 public class Mt {
     final int N = 624;
+    final int W = 32;
+    final int M = 397;
+    final int R = 31;
+    final int A = 0x9908B0DF;
+    final int U = 11;
+    final int D = 0xFFFFFFFF;
+    final int S = 7;
+    final int B = 0x9D2C5680;
+    final int T = 15;
+    final int C = 0xEFC60000;
+    final int L = 18;
 
     int[] MT = new int[N];
 }
