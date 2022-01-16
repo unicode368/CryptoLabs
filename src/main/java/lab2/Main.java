cab4953e686ac711743f11437bcc5c0de7e73926
lamecode <kislaya.sliva@gmail.com>
2022-01-16 10:23:10 +0200

    adding candidates here

--- src/main/java/lab2/Main.java
+++ src/main/java/lab2/Main.java
@@ -23,6 +23,7 @@ public class Main {
                 "2f0cdfe46d35498602e9df91f9c842061d569a6adbd8e701579397ac82d093f12c09034696787f0a\n" +
                 "390bcfac282f558a03b9df9dedcc43425244d268c0cfa61602918cbd848481bf3c5c1c47db7c660c63\n" +
                 "2f0cdfe464344e8650edc59daac3504b1710d56b89dce5011e8c90f6\n").split("\n");
+        String[] candidates = new String[cipheredText.length];
         for (int i = 0; i < cipheredText.length - 1; i++) {
             String xoredEncryptedStrings = new String(xorWithKey(cipheredText[i].getBytes(),
                     cipheredText[i + 1].getBytes()), StandardCharsets.UTF_8);
